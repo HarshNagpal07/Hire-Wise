a13953542ab2e5d13b8c69f7e04869f118d5b9f2
Harsh056-star <hnagpal056@gmail.com>
2025-08-19 23:55:08 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets: ["latin"]});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,10 +14,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
+
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -44,5 +40,6 @@ export default function RootLayout({ children }) {
           </ThemeProvider>
       </body>
     </html>
+            </ClerkProvider>
   );
 }
