11fe52a5c0385b1fb9c17ecd09a33d02b376216a
Harsh056-star <hnagpal056@gmail.com>
2025-09-06 23:30:54 +0530

    Setting icons

--- components/header.jsx
+++ components/header.jsx
@@ -5,43 +5,63 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { LayoutDashboard } from "lucide-react";
+import { ChevronDown, LayoutDashboard, StarIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 const Header = () => {
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
-         <Link href="/"> {/*Anchor tag for home page */}
-        <Image 
-        src="/logo.png" 
-        alt="Hire Wise Logo" 
-        width={200} 
-        height={60}
-        className="h-20 py-1 w-auto object-contain"
-        />
+        <Link href="/">
+          {" "}
+          {/*Anchor tag for home page */}
+          <Image
+            src="/logo.png"
+            alt="Hire Wise Logo"
+            width={200}
+            height={60}
+            className="h-20 py-1 w-auto object-contain"
+          />
         </Link>
         <div>
           <SignedIn>
             <Link href={"/dashboard"}>
-            <Button>
-              <LayoutDashboard className="h-4 w-4"/>
-              Industry Insights
-            </Button>
+              <Button>
+                <LayoutDashboard className="h-4 w-4" />
+                <span className="hidden md:block">Industry Insights</span>
+              </Button>
             </Link>
           </SignedIn>
+
+          <DropdownMenu>
+
+            <DropdownMenuTrigger>
+              <Button>
+                <StarIcon className="h-4 w-4" />
+                <span className="hidden md:block">Growth Tools</span>
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
         </div>
       </nav>
-      
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
 
-        <SignedIn>
-          <UserButton/>
-        </SignedIn>
-      
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </header>
-   
   );
 };
 
